c19e9e14125fb3872f637f0ec04cd7f4343963b5
Jae <giffordjacob0@gmail.com>
2023-04-29 14:02:04 +0800

    final build I hope

--- src/components/effects/Background.tsx
+++ src/components/effects/Background.tsx
@@ -1,16 +1,16 @@
 import style from './effects.module.css';
-
+const baseURL = import.meta.env.VITE_BASE_URL;
 function Background() {
   return (
     <div className='relative w-screen h-screen overflow-hidden'>
       <img
         className={`w-[2160px] object-cover max-h-[3840px] min-h-full ${style.animation} ${style.colorimg} absolute`}
-        src='assets/images/graffiti-grey.png'
+        src={`${baseURL}/assets/images/graffiti-grey.png`}
         alt='background grey'
       ></img>
       <img
         className='w-[2160px] object-cover max-h-[3840px] min-h-full block'
-        src='assets/images/graffiti-original.jpg'
+        src={`${baseURL}/assets/images/graffiti-original.jpg`}
         alt='background color'
       ></img>
     </div>
