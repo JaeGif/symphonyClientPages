29c8cafd2d9d85f04d1403c9166bff3137c6686c
Jae <giffordjacob0@gmail.com>
2023-04-29 11:24:51 +0800

    live on pages

--- src/components/effects/Background.tsx
+++ src/components/effects/Background.tsx
@@ -5,12 +5,12 @@ function Background() {
     <div className='relative w-screen h-screen overflow-hidden'>
       <img
         className={`w-[2160px] object-cover max-h-[3840px] min-h-full ${style.animation} ${style.colorimg} absolute`}
-        src='/assets/images/graffiti-grey.png'
+        src='assets/images/graffiti-grey.png'
         alt='background grey'
       ></img>
       <img
         className='w-[2160px] object-cover max-h-[3840px] min-h-full block'
-        src='/assets/images/graffiti-original.jpg'
+        src='assets/images/graffiti-original.jpg'
         alt='background color'
       ></img>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/symphonyClientPages/'>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
