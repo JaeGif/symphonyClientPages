91e49192ebeca31fc8c36cfac872af71bc1e4b88
Jae <giffordjacob0@gmail.com>
2023-04-29 14:21:20 +0800

    register images readd

--- src/components/effects/RegisterBackground.tsx
+++ src/components/effects/RegisterBackground.tsx
@@ -1,16 +1,17 @@
 import style from './effects.module.css';
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 function RegisterBackground() {
   return (
     <div className='relative w-screen h-screen overflow-hidden'>
       <img
         className={`w-[2160px] object-cover max-h-[3840px] min-h-full ${style.registerAnimation} ${style.registerImg} absolute`}
-        src='/assets/images/registerBackground.png'
+        src={`${baseURL}/assets/images/registerBackground.png`}
         alt='background grey'
       ></img>
       <img
         className='w-[2160px] object-cover max-h-[3840px] min-h-full block'
-        src='/assets/images/registerOriginal.png'
+        src={`${baseURL}/assets/images/registerOriginal.png`}
         alt='background color'
       ></img>
     </div>
